9a827eaacd3f35e941d44241ccc28c6ac12edd2c
Felix <martinez.alvaro.felix@gmail.com>
2015-09-29 13:04:28 +0200

    Calculator

--- JavaScript/Funciones/Funciones.js
+++ JavaScript/Funciones/Funciones.js
@@ -11,21 +11,38 @@ function saludando(texto) {
 
 }
 
-//Funcion anonima
-document.getElementById("btn1").onclick = function (e) {
-    alert("Hola" + e.target.outerText);
-}
-
 document.getElementById("btn2").onclick = saludando;
+
+
+
+//Funcion autoinvocada
+(function () {
+
+    //Funcion anonima
+    document.getElementById("btn1").onclick = function (e) {
+        alert("Hola" + e.target.outerText);
+    }
+
+})();
+
 
 /*
     Diferencias entre poner parentesis al asignar una funcion a un boton y no ponerlo.
     Ej:
         document.getElementById("btn2").onclick = saludo;
 
-            -Aqui se asignaria al boton el codigo de la funcion. Y al pulsar el boton se ejecutaria el codigo.
+            -Aqui se asignaria al boton el codigo de la funcion. Y al pulsar el boton SI se ejecutaria el codigo.
 
         document.getElementById("btn2").onclick = saludo();
 
-            -Aqui se ejecutaria al cargar la pagina el codigo. Y al pulsar el boton no se ejecutaria el codigo
+            -Aqui se ejecutaria el codigo al cargar la pagina. Y al pulsar el boton NO se ejecutaria el codigo
+
+
+    En una funcion los parametros pueden ser: e y evt.
+        - 'e' es el delegado del evento.
+        - 'target' es el objeto que contiene el evento con toda su informacion, en este caso el boton.
+
+
+    Autoinvocacion de funciones. Se introduce la funcion dentro de parentesis y finaliza con parentesis.
+    Se utiliza para crear objetos.
 */
---
+++ JavaScript/Funciones/Practica/funciones.js
@@ -0,0 +1,52 @@
+﻿
+var validar = function (e) {
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+
+    if (!isNaN(num1.value) && !isNaN(num2.value)) {
+
+        calculo(e);
+
+    }
+
+}
+
+var calculo = function (e) {
+    //Asigno a las variables num1 y num2 el value de los input
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    //Asigno a la variable operando el contenido del boton (+ | - | * | / )
+    var operando = e.target.innerHTML;
+    
+    //Concateno en operando el contenido de num1, operando y num2
+    var operacion = num1 + operando + num2;
+
+    var resultado = eval(operacion);
+
+    document.getElementById("operando").innerHTML = operando;
+    document.getElementById("igual").innerHTML = "=";
+    document.getElementById("resultado").value = resultado;
+
+}
+/*
+    Funcion que limpia el contenido de los input y cajas.
+*/
+var clear = function () {
+
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("operando").innerHTML = "";
+    document.getElementById("igual").innerHTML = "";
+    document.getElementById("resultado").value = "";
+}
+
+
+var invocar = (function () {
+
+    document.getElementById("suma").onclick = validar;
+    document.getElementById("resta").onclick = validar;
+    document.getElementById("multiplicacion").onclick = validar;
+    document.getElementById("division").onclick = validar;
+    document.getElementById("limpiar").onclick = clear;
+
+})();
